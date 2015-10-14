2645ccf858ac81e823aef2517fcc624ff62b403a
Daria <dariaboom@yahoo.com>
2015-10-14 16:07:23 +0300

    Change default values, and refactor for loop.

--- TestingGit/src/SalesData.java
+++ TestingGit/src/SalesData.java
@@ -1,10 +1,15 @@
 
 public class SalesData {
 	
-	int data[] = {0, 4 , 42};
+	int data[] = {-1, 0, 4 , 42, 84};
 
 	public void display() {
-		System.out.println("todo:implement data display");
+		System.out.println("Data");
+		
+		for (int i : data) {
+			
+			System.out.println("Next value:" + data[i]);
+		}
 		
 	}
 
