640503cd67e9355b643a3fe1569572c327ac0b1a
Daria <dariaboom@yahoo.com>
2015-10-14 15:01:58 +0300

    Commit A

---
+++ TestingGit/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("hello happy sales people!");
+		System.out.println("thios app shows sales data");
+	}
+
+}
